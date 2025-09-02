14ce52b947372eae5f591cafb5be8bb0558fc285
Gabe Rudy <rudy@goldenhelix.com>
2025-09-02 08:36:40 -0600

    Only install vsproject-template

--- install.sh
+++ install.sh
@@ -1,3 +1,1 @@
-cp -Rf PacBio_PureTarget_Report "$WORKSPACE_DIR/AppData/VarSeq/User Data/ReportTemplates/"
 cp -f *.vsproject-template "$WORKSPACE_DIR/AppData/VarSeq/User Data/ProjectTemplates/"
-cp -f Meta/*.tsf "$WORKSPACE_DIR/AppData/Common Data/UserAnnotations/"
